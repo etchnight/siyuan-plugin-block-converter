ad12d3bc5a7e1bdbbceef5cdc99da7955d1c2e20
dualwind <dualwind@qq.com>
2024-02-20 21:19:00 +0800

    feat: 生成流程图

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Plugin, Menu, Protyle } from "siyuan";
 import { buildSyTableBlocks } from "./libs/tableTransfer";
 import { insertBlock } from "../subMod/siyuanPlugin-common/siyuan-api/block";
-
+import {
+  queryBlockById,
+  queryRefInfoById,
+} from "../subMod/siyuanPlugin-common/siyuan-api/query";
 export default class PluginTableImporter extends Plugin {
   private blockIconEventBindThis = this.blockIconEvent.bind(this);
   async onload() {
@@ -21,8 +24,102 @@ export default class PluginTableImporter extends Plugin {
     detail: { menu: Menu; blockElements: [HTMLElement]; protyle: Protyle };
   }) {
     this.htmlBlock2tableBlock(detail);
+    this.block2flowchart(detail);
   }
 
+  private async block2flowchart(detail: {
+    menu: Menu;
+    blockElements: [HTMLElement];
+    protyle: Protyle;
+  }) {
+    if (detail.blockElements.length !== 1) {
+      return;
+    }
+    const selectElement = detail.blockElements[0];
+    const blockId = selectElement.getAttribute("data-node-id");
+    detail.menu.addItem({
+      iconHTML: "",
+      label: "生成流程图",
+      click: async () => {
+        let flowchartText = "flowchart LR";
+        let count = 0;
+        const startBlock = await queryBlockById(blockId);
+        let queue = [startBlock];
+        let idList = []; //已完成的，防止重复
+        while (queue.length !== 0 && count < 100) {
+          let block = queue.pop();
+          const id = block.id;
+          idList.push(id);
+          const refInfos = await queryRefInfoById(id);
+          let flowRefs: string[] = [];
+          for (let item of refInfos) {
+            if (item.content.startsWith("-&gt;")) {
+              flowchartText +=
+                "\n" + buildFlowEdge(id, item.def_block_id, item.content);
+              flowRefs.push(item.content);
+              if (!idList.includes(item.def_block_id)) {
+                let defBlock = await queryBlockById(item.def_block_id);
+                queue.push(defBlock);
+              }
+            }
+          }
+          flowchartText += "\n" + buildFlowNode(id, block.content, flowRefs);
+        }
+        //console.log(flowchartText);
+        await insertBlock({
+          dataType: "markdown",
+          data: "```" + "mermaid" + "\n" + flowchartText + "\n" + "```",
+          previousID: blockId,
+        });
+        function buildFlowId(id: BlockId) {
+          return "a" + id.replace("-", "");
+        }
+        function buildFlowNode(
+          id: BlockId,
+          markdown: string,
+          flowRefs: string[]
+        ) {
+          //markdown转义
+          let result = markdown.replace(/\"/g, "#quot;");
+          result = result.replace(/\'/g, "#quot;");
+          for (let content of flowRefs) {
+            result = result.replace(content, "");
+          }
+          //节点形状
+          const prefix = flowRefs.length > 1 ? "{{" : "[";
+          const suffix = flowRefs.length > 1 ? "}}" : "]";
+          return buildFlowId(id) + `${prefix}\"\`${result}\`\"${suffix}`;
+        }
+        function buildFlowEdge(
+          id: BlockId,
+          targetId: BlockId,
+          refContent: string
+        ) {
+          let textOnArrow = "";
+          //const arrowText = ["是", "yes", "否", "no"];
+          /**
+           * @param isMatch 在match中使用为ture，在search中使用为false
+           * @returns 箭头+中英文括号+括号内内容+中英文括号，isMatch为ture时使用预查
+           */
+          const regexFunc = (isMatch?: boolean) => {
+            const prefix = isMatch ? "?<=" : "";
+            const suffix = isMatch ? "?=" : "";
+            return new RegExp(
+              `(${prefix}-&gt;(\\(|（))(.*?)(${suffix}(\\)|）))`,
+              "g"
+            );
+          };
+          const index = refContent.toLowerCase().search(regexFunc());
+          if (index === 0) {
+            const matchResult = refContent.toLowerCase().match(regexFunc(true));
+            textOnArrow = "|" + matchResult[0] + "|";
+          }
+          ` A-->|text|B`;
+          return `${buildFlowId(id)} -->${textOnArrow}${buildFlowId(targetId)}`;
+        }
+      },
+    });
+  }
   private htmlBlock2tableBlock = (detail: {
     menu: Menu;
     blockElements: [HTMLElement];
